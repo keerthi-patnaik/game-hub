2018326d04b1aba64962cdca60c16bc8fee5b15d
Keerthi <keerthi@byond.travel>
2024-03-08 17:43:44 -0600

    Added grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `'nav'
+            'main'`,
+        lg: `'nav nav nav'
+          'side-nav main main'`,
+      }}
+    >
+      <GridItem bg="coral" area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="pink" area="side-nav">
+          Side Nav
+        </GridItem>
+      </Show>
+      <GridItem bg="green" area="main">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
