7075fe9961a6dd286ad477f0ec9f1578802a2391
Keerthi <keerthi@byond.travel>
2024-03-09 15:14:45 -0600

    implemented enviroment

--- src/services/api-clients.ts
+++ src/services/api-clients.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'f878673c100f4dd49fee041a497af1e0',
+    key: import.meta.env.VITE_RAWG_KEY,
   },
 });
