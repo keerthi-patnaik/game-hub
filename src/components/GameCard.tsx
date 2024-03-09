a25b73eef884703d0c32914addd4631d9f5183f9
Keerthi <keerthi@byond.travel>
2024-03-09 13:32:47 -0600

    added came card to display fetched games

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,17 @@
+import { Card, CardBody, Heading, Stack } from '@chakra-ui/react';
+
+type GameCardProps = {};
+
+const GameCard = () => {
+  return (
+    <Card>
+      <CardBody>
+        <Stack>
+          <Heading></Heading>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
